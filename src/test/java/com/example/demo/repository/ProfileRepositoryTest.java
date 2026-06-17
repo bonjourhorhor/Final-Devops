4686ef70a183d544ae9a26d0ca8b8648bd1057d4
bonjourhorhor <luylyhor28@gmail.com>
2026-06-17 18:04:51 +0700

    Fix ProfileRepositoryTest to use SQLite instead of H2

--- src/test/java/com/example/demo/repository/ProfileRepositoryTest.java
+++ src/test/java/com/example/demo/repository/ProfileRepositoryTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,12 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:repotest;DB_CLOSE_DELAY=-1",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
+
 class ProfileRepositoryTest {
 
     @Autowired
